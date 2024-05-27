830b2bd7466ebfe82e4c3f3e09afc9ca50f1c71c
Linal Punchihewa <147425069+LinalPunchihewa@users.noreply.github.com>
2024-05-27 12:07:22 +0000

    qwqw

---
+++ .devcontainer/member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    const member = {
+        name: 'Luis G. Santamaría',
+        role: 'Software Engineer',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'MongoDB', 'Python', 'Django'],
+        techCommunities: {
+            coorganizer: "Noders",
+            speaker: "JSConf"
+        }
+    }
+}
